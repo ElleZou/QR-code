ec40301f820986dfa46ff54ef6e3b7723429c88c
Elle Z <elle.zou@outlook.com>
2024-05-15 21:59:15 +0800

    install package

--- qrcode_generator.py
+++ qrcode_generator.py
@@ -1,2 +1,5 @@
 import io
 import sys
+import qrcode
+from PyQt5 import QtWidgets, QtGui
+from PyQt5.QtWidgets import QApplication, QGridLayout, QLabel, QLineEdit, QPushButton, QComboBox, QSpinBox, QFileDialog, QDialog
